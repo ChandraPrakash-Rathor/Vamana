ca0772076e399a309a2a2a05b16249af05205534
Chandraprakash Rathor <ts.chandra111@gmail.com>
2026-03-24 12:42:15 +0530

    by cp

--- perfume-shop/admin/src/APIS/apis/config.js
+++ perfume-shop/admin/src/APIS/apis/config.js
@@ -1,7 +1,7 @@
 // API Base URL Configuration
-// export const baseUrl = 'https://admin.vamana.store/api/admin/';
-// export const imageBaseUrl = 'https://admin.vamana.store';
+export const baseUrl = 'https://admin.vamana.store/api/admin/';
+export const imageBaseUrl = 'https://admin.vamana.store';
 
 // For development, uncomment below:
-export const baseUrl = 'http://localhost:5000/api/admin/';
-export const imageBaseUrl = 'http://localhost:5000';
+// export const baseUrl = 'http://localhost:5000/api/admin/';
+// export const imageBaseUrl = 'http://localhost:5000';
--- perfume-shop/member/src/redux/apis/config.js
+++ perfume-shop/member/src/redux/apis/config.js
@@ -1,7 +1,7 @@
 // API Base URL Configuration
-// export const baseUrl = 'https://vamana.store/api/member/';
-// export const imageBaseUrl = 'https://vamana.store';
+export const baseUrl = 'https://vamana.store/api/member/';
+export const imageBaseUrl = 'https://vamana.store';
 
 // For development, uncomment below:
-export const baseUrl = 'http://localhost:5000/api/member/';
-export const imageBaseUrl = 'http://localhost:5000';
+// export const baseUrl = 'http://localhost:5000/api/member/';
+// export const imageBaseUrl = 'http://localhost:5000';
